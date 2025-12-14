2310e1c1d2e6dfd6e0f0878790a83659a6c44373
João Gabriel da Cunha Vasconcellos <122104633+JoaoGabrielCunha@users.noreply.github.com>
2025-12-14 17:32:47 -0300

    Add files via upload

---
+++ Projeto08/src/08b_prototipo.cpp
@@ -0,0 +1,341 @@
+#include <Matter.h>
+#include <GFButton.h>
+#include <Adafruit_BME680.h>
+#include <Preferences.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <GxEPD2_BW.h>
+#include <U8g2_for_Adafruit_GFX.h>
+#include <GxEPD2_BW.h>
+#include <BarcodeGFX.h>
+#include <QRCodeGFX.h>
+#include <LittleFS.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include "certificados.h"
+#include <MQTT.h> 
+
+MatterColorLight luzRGBMatter;
+MatterOccupancySensor movimentoMatter;
+MatterHumiditySensor umidadeMatter;
+MatterTemperatureSensor temperaturaMatter;
+MatterThermostat termostatoMatter;
+
+MatterGenericSwitch botaoMatter;
+
+GFButton sensor(21);
+
+Adafruit_BME680 sensorBME;
+
+float temperatura;
+float umidade;
+
+GFButton botao_cinco(5);
+
+int count = 0;
+
+unsigned long instanteAnterior = 0;
+
+Preferences preferencias;
+
+WebServer servidor(80);
+
+U8G2_FOR_ADAFRUIT_GFX fontes;
+GxEPD2_290_T94_V2 modeloTela(10, 14, 15, 16);
+GxEPD2_BW<GxEPD2_290_T94_V2, GxEPD2_290_T94_V2::HEIGHT> tela(modeloTela);
+
+BarcodeGFX codigoBarras(tela);
+QRCodeGFX qrcode(tela);
+
+int numero_desatualizado_conexões;
+
+String nome_Rede_Wifi_Ap = "";
+String Senha_Wifi = "";
+
+bool modoSetup = true;
+
+JsonDocument dados;
+String String_Dados;
+
+bool mudouLuz(bool acender, espHsvColor_t corHSV)
+{
+  espRgbColor_t corRGB = espHsvColorToRgbColor(corHSV);
+  // acende ou apaga a sua luz de acordo com variável acender
+  if (acender == true)
+  {
+    rgbLedWrite(RGB_BUILTIN, corRGB.r, corRGB.g, corRGB.b);
+    acender = false;
+    dados["aceso"] = false;
+  }
+  else
+  {
+    rgbLedWrite(RGB_BUILTIN, 0, 0, 0);
+    acender = true;
+    dados["aceso"] = true;
+  }
+
+  dados["numero"] = 5;
+  serializeJson(dados, String_Dados);
+
+  
+
+  return true;
+}
+
+void movimento(GFButton &sensor)
+{
+  Serial.println("Movimento detectado!");
+  // movimentoMatter.setOccupancy(true);
+}
+
+void inercia(GFButton &sensor)
+{
+  Serial.println("Inércia detectada!");
+  // movimentoMatter.setOccupancy(false);
+}
+
+void recomissionarMatter()
+{
+  if (!Matter.isDeviceCommissioned())
+  {
+    Serial.println("Procurando ambiente Matter");
+    Serial.printf("Código de pareamento: %s\r\n", Matter.getManualPairingCode().c_str());
+    Serial.printf("Site com QR code de pareamento: %s\r\n", Matter.getOnboardingQRCodeUrl().c_str());
+    Serial.print("Procurando ambiente Matter...");
+    while (!Matter.isDeviceCommissioned())
+    {
+      Serial.print(".");
+      delay(1000);
+    }
+    Serial.println("\nConectado no ambiente Matter!");
+  }
+} 
+
+void botaoPressionado(GFButton &botaoDoEvento)
+{
+  Serial.println("Botão foi pressionado!");
+  count += 1;
+  Serial.print("Contagem: ");
+  Serial.println(count);
+  preferencias.putInt("contagem", count);
+}
+
+void botaoSolto(GFButton &botaoDoEvento)
+{
+  Serial.println("Botão foi solto!");
+}
+
+void pagina1()
+{
+  servidor.send(200, "text/html", "João Gabriel!");
+}
+
+void pagina2()
+{
+  File arquivo = LittleFS.open("/ajustes.html", "r");
+  if (!arquivo)
+  {
+    servidor.send(500, "ajustes/html", "Erro no HTML");
+    return;
+  }
+  String html = arquivo.readString();
+  arquivo.close();
+  html.replace("{{nomeRedeWiFi}}", nome_Rede_Wifi_Ap);
+  html.replace("{{senhaWiFi}}", Senha_Wifi);
+  servidor.send(200, "text/html", html);
+}
+
+void tratarDados()
+{
+  nome_Rede_Wifi_Ap = servidor.arg("nomeRedeWiFi");
+  Senha_Wifi = servidor.arg("senhaWiFi");
+  Serial.println(nome_Rede_Wifi_Ap);
+  Serial.println(Senha_Wifi);
+
+  preferencias.putString("nome", nome_Rede_Wifi_Ap);
+  preferencias.putString("Senha", Senha_Wifi);
+
+  servidor.sendHeader("Location", "/ajustes");
+  servidor.send(303);
+}
+
+void botaoApertado(GFButton &botaoDoEvento)
+{
+  if (modoSetup == true)
+  {
+    modoSetup = false;
+    preferencias.putBool("modo_da_setup", false);
+  }
+  else
+  {
+    modoSetup = true;
+    preferencias.putBool("modo_da_setup", true);
+  }
+  esp_restart();
+}
+
+void reconectarWiFi()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    WiFi.begin(nome_Rede_Wifi_Ap, Senha_Wifi);
+    Serial.print("Conectando ao WiFi...");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      Serial.print(".");
+      delay(1000);
+    }
+    Serial.print("conectado!\nEndereço IP: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(500);
+
+  tela.init();
+  tela.setRotation(3);
+  tela.fillScreen(GxEPD_WHITE);
+  tela.display(true);
+
+  fontes.begin(tela);
+  fontes.setForegroundColor(GxEPD_BLACK);
+
+  fontes.setFont(u8g2_font_helvB10_te);
+  fontes.setFontMode(1);
+  fontes.setCursor(15, 20);
+  fontes.print("Conecte-se ao WiFi no QR Code abaixo");
+
+  qrcode.setScale(2);
+  qrcode.draw("WIFI:S:Joao_Gabriel_Cunha;T:WPA;P:12345678;;", 100, 40);
+
+  tela.display(true);
+
+  botaoMatter.begin();
+  
+  sensor.setPressHandler(inercia);
+  sensor.setReleaseHandler(movimento);
+
+  if (!sensorBME.begin())
+  {
+    Serial.println("Erro no sensor BME");
+    while (true)
+      ;
+  }
+
+  sensorBME.setTemperatureOversampling(BME680_OS_8X);
+  sensorBME.setHumidityOversampling(BME680_OS_2X);
+  sensorBME.setPressureOversampling(BME680_OS_4X);
+  sensorBME.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  sensorBME.setGasHeater(320, 150); // ˚C e ms, (0, 0) para desativar
+
+  botao_cinco.setPressHandler(botaoPressionado);
+  botao_cinco.setReleaseHandler(botaoSolto);
+
+  preferencias.begin("JG_Cunha"); // nome único seu
+
+  count = preferencias.getInt("contagem", 0);
+  nome_Rede_Wifi_Ap = preferencias.getString("nome", "");
+  Senha_Wifi = preferencias.getString("Senha", "");
+  modoSetup = preferencias.getBool("modo_da_setup", true);
+
+  if (!LittleFS.begin())
+  {
+    Serial.println("LittleFS falhou!");
+    while (true)
+    {
+    };
+
+    // Matter.begin();
+    // recomissionarMatter();
+  }
+
+  botao_cinco.setHoldHandler(botaoApertado);
+
+  if (modoSetup == true)
+  {
+    WiFi.softAP("Joao_Gabriel_Cunha", "12345678");
+    Serial.println("WiFi Access Point iniciado!");
+    Serial.print("IP: " + WiFi.softAPIP().toString());
+
+    servidor.on("/pagina1", HTTP_GET, pagina1);
+    servidor.on("/ajustes", HTTP_GET, pagina2);
+    servidor.on("/ajustes", HTTP_POST, tratarDados);
+
+    servidor.begin();
+  }
+
+  else
+  {
+    reconectarWiFi();
+
+    movimentoMatter.begin();
+    temperaturaMatter.begin();
+    luzRGBMatter.begin();
+    luzRGBMatter.onChange(mudouLuz);
+    umidadeMatter.begin();
+    termostatoMatter.begin();
+    Matter.begin();
+    recomissionarMatter();
+    
+  }
+}
+
+void loop()
+{
+
+  sensor.process();
+
+  unsigned long instanteAtual = millis();
+  if (instanteAtual > instanteAnterior + 10000)
+  {
+
+    Serial.println("+10 segundo");
+
+    sensorBME.performReading();
+    temperatura = sensorBME.temperature;
+    umidade = sensorBME.humidity;
+
+    instanteAnterior = instanteAtual;
+  }
+
+  botao_cinco.process();
+
+  if (modoSetup == true)
+  {
+    servidor.handleClient();
+    int numeroDeConexoes = WiFi.softAPgetStationNum();
+
+    int diferenca_de_conexoes;
+    diferenca_de_conexoes = numeroDeConexoes - numero_desatualizado_conexões;
+
+    if (diferenca_de_conexoes != 0)
+    {
+      tela.fillScreen(GxEPD_WHITE);
+
+      fontes.setForegroundColor(GxEPD_BLACK);
+
+      fontes.setFont(u8g2_font_helvB10_te);
+      fontes.setFontMode(1);
+      fontes.setCursor(0, 20);
+      fontes.print("Agora acesse a página no QR Code abaixo");
+
+      qrcode.setScale(2);
+      qrcode.draw("http://192.168.4.1/ajustes", 100, 40);
+
+      tela.display(true);
+
+      numero_desatualizado_conexões = numeroDeConexoes;
+    }
+  }
+  else
+  {
+    temperaturaMatter.setTemperature(temperatura);
+    umidadeMatter.setHumidity(umidade);
+    reconectarWiFi();
+  }
+
+  // recomissionarMatter();
+}
